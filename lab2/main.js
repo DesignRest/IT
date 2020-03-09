0bb86e955ebda962ed876cfb33faf470dfc5fec8
DesignRest <60433215+DesignRest@users.noreply.github.com>
2020-03-09 12:31:33 +0600

    Add files via upload

---
+++ lab2/main.js
@@ -0,0 +1,6 @@
+function btn() {
+	
+		$("input[id='l']").css("border", "4px solid green");
+		$("input[id='p']").css("border", "4px solid red");
+		$("input[id='pp']").css("border", "4px solid blue");
+}
